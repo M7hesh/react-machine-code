54ec2a4faf19d99f5c58080930c222fcceb0c7c1
Mahesh Mote <44047896+M7hesh@users.noreply.github.com>
2025-04-20 00:46:15 +0530

    add progress bar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
+import { useEffect } from "react";
 import "./styles.css";
 import useToast from "./toast/useToast";
+import ProgressBar from "./progress-bar/ProgressBar";
 
 export default function App() {
-  const { toastHandler, ToastComponent } = useToast()
+  const { toastHandler, ToastComponent } = useToast();
 
   const notificationHandler = (type) => {
-    toastHandler({ type, message: type, duration: 5000 })
-  }
+    toastHandler({ type, message: type, duration: 5000 });
+  };
+
+  const toast = (
+    <div>
+      <button onClick={() => notificationHandler("success")}>Success</button>
+      <button onClick={() => notificationHandler("error")}>Error</button>
+      <button onClick={() => notificationHandler("info")}>Info</button>
+      <button onClick={() => notificationHandler("warning")}>Warning</button>
+      {ToastComponent}
+    </div>
+  );
+
+  useEffect;
+
   return (
     <div className="App">
-      <button onClick={() => notificationHandler('success')}>Success</button>
-      <button onClick={() => notificationHandler('error')}>Error</button>
-      <button onClick={() => notificationHandler('info')}>Info</button>
-      <button onClick={() => notificationHandler('warning')}>Warning</button>
-      {ToastComponent}
+      <ProgressBar />
     </div>
   );
 }
